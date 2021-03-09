75fc81b6fb98ae1c9059c873610773066504dcdd
Jhonatan Medeiros <jhonatanmedeiros7@gmail.com>
2021-03-09 16:52:36 -0300

    Introdução ao C#

---
+++ intro-csharp-balta/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace intro_csharp_balta
+{
+    class Program
+    {
+        private const bool V = true;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+
+            var cidades = new List<string>
+            {
+                "São Paulo",
+                "São Bernardo",
+                "Curitiba"
+            };
+
+            var filtradas = cidades.Where(x => x.StartsWith("C"));
+
+            foreach (var cidade in filtradas)
+            {
+                Console.WriteLine(cidade);
+            }
+
+            // Dictionary = Map
+            var capitais = new Dictionary<string, string>();
+
+            capitais.Add("RN", "Natal");
+            capitais.Add("PR", "Curitiba");
+
+            Console.WriteLine(capitais["PR"]);
+
+
+            // Linq
+            var estados = capitais.Keys.Where(x => x.StartsWith("R"));
+
+            foreach (var uf in estados)
+            {
+                Console.WriteLine(uf);
+            }
+
+            Console.Read();
+        }
+    }
+}
